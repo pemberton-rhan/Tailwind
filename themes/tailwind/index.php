3eb6e7421cb1a047d24261e2b794662c334a578e
Rhan Pemberton <rhanpemberton@rhans-macbook-pro.local>
2025-08-12 12:29:39 -0500

    Made change to conflict branch.

--- themes/tailwind/index.php
+++ themes/tailwind/index.php
@@ -27,7 +27,7 @@
 		</div>
 		
 		<!-- Component Tailwind -->
-		<div id="cta-tailwind" class="flex justify-center bg-llc-blue font-poppins w-[350px] p-[15px] relative shadow-custom rounded-sm">
+		<div id="cta-tailwind" class="flex justify-center bg-llc-blue font-poppins w-[350px] p-[15px] relative shadow-custom rounded-sm mb-[50px]">
 			<div class="inner">
 				<svg class="absolute top-[10px] right-[10px] w-[17px] height-[17px] hover:cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 					<path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -44,6 +44,8 @@
 				</div>
 			</div>
 		</div>
+		
+		<h3>This is a change from branch-conflict</h3>
 			
 	</body>
 </html>
